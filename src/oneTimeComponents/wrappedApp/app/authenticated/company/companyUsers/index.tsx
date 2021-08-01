4f4b6beaeaa4826387ad700e1e70d9b7d51eb756
Chase <chasejarms@gmail.com>
2021-08-01 13:15:36 -0600

    Start work on adding company admin rights

--- src/oneTimeComponents/wrappedApp/app/authenticated/company/companyUsers/index.tsx
+++ src/oneTimeComponents/wrappedApp/app/authenticated/company/companyUsers/index.tsx
@@ -1,12 +1,19 @@
 /** @jsxImportSource @emotion/react */
 import { jsx, css } from "@emotion/react";
 import {
+    Checkbox,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    IconButton,
     Paper,
     Table,
     TableBody,
     TableCell,
     TableHead,
     TableRow,
+    Typography,
 } from "@material-ui/core";
 import { useEffect, useState } from "react";
 import { Api } from "../../../../../../api";
@@ -14,7 +21,8 @@ import { BoardsContainer } from "../../../../../../components/boardsContainer";
 import { CenterLoadingSpinner } from "../../../../../../components/centerLoadingSpinner";
 import { useAppRouterParams } from "../../../../../../hooks/useAppRouterParams";
 import { IUser } from "../../../../../../models/user";
-import { Check } from "@material-ui/icons";
+import { Check, Edit } from "@material-ui/icons";
+import { WrappedButton } from "../../../../../../components/wrappedButton";
 
 export function CompanyUsers() {
     const { companyId } = useAppRouterParams();
@@ -43,6 +51,17 @@ export function CompanyUsers() {
         };
     }, []);
 
+    const [userToUpdate, setUserToUpdate] = useState<null | IUser>(null);
+    const dialogIsOpen = !!userToUpdate;
+    function onCloseDialog() {
+        setUserToUpdate(null);
+    }
+    function onClickToggleUserRights(user: IUser) {
+        return () => {
+            setUserToUpdate(user);
+        };
+    }
+
     const classes = createClasses();
     return (
         <BoardsContainer>
@@ -62,9 +81,14 @@ export function CompanyUsers() {
                                         <TableRow key={user.itemId}>
                                             <TableCell>{user.name}</TableCell>
                                             <TableCell>
-                                                {user.isCompanyAdmin ? (
-                                                    <Check />
-                                                ) : null}
+                                                <Checkbox
+                                                    checked={
+                                                        user.isCompanyAdmin
+                                                    }
+                                                    onChange={onClickToggleUserRights(
+                                                        user
+                                                    )}
+                                                />
                                             </TableCell>
                                         </TableRow>
                                     );
@@ -72,6 +96,43 @@ export function CompanyUsers() {
                             </TableBody>
                         </Table>
                     </Paper>
+                    <Dialog
+                        open={dialogIsOpen}
+                        onClose={onCloseDialog}
+                        disableBackdropClick={false}
+                    >
+                        <DialogTitle>
+                            {userToUpdate?.isCompanyAdmin
+                                ? "Remove Company Admin Rights"
+                                : "Enable Company Admin Rights"}
+                        </DialogTitle>
+                        <DialogContent>
+                            <Typography>
+                                {userToUpdate?.isCompanyAdmin
+                                    ? "Are you sure you want to remove company admin rights from this user?"
+                                    : "Are you sure you want to give this user company admin rights? They will access to all company features including all boards, user rights, and the ability to delete the company account."}
+                            </Typography>
+                        </DialogContent>
+                        <DialogActions>
+                            <WrappedButton
+                                onClick={onCloseDialog}
+                                disabled={false}
+                            >
+                                Close
+                            </WrappedButton>
+                            <WrappedButton
+                                variant="contained"
+                                onClick={() => null}
+                                color="primary"
+                                disabled={false}
+                                showSpinner={false}
+                            >
+                                {userToUpdate?.isCompanyAdmin
+                                    ? "Remove Company Admin Rights"
+                                    : "Enable Company Admin Rights"}
+                            </WrappedButton>
+                        </DialogActions>
+                    </Dialog>
                 </div>
             ) : (
                 <CenterLoadingSpinner size="large" />
