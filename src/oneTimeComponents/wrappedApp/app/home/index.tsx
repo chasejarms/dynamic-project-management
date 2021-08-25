de95d8cadf0a0dc43bef48e08014f832303ef9a8
Chase <chasejarms@gmail.com>
2021-08-25 09:53:29 -0600

    Refine the text for the landing page

--- src/oneTimeComponents/wrappedApp/app/home/index.tsx
+++ src/oneTimeComponents/wrappedApp/app/home/index.tsx
@@ -108,7 +108,7 @@ export function Home() {
                 title={"Prioritization is Tough"}
                 textSections={[
                     "Prioritizing large projects is incredibly challenging. Between the backlog and the in progress board, there can be hundreds of individual tickets that the team needs to prioritize.",
-                    "And if that weren’t enough, stakeholders are often tweaking the direction of the project forcing them team to reorganize the board again and again. The more stakeholders that are involved, the more cumbersome this process becomes.",
+                    "And as if that weren’t enough, stakeholders are often tweaking the direction of the project forcing the team to reorganize the board again and again. The more stakeholders that are involved, the more cumbersome this process becomes.",
                 ]}
                 placeContent="left"
                 wrappedButtonProps={wrappedButtonProps}
@@ -117,8 +117,8 @@ export function Home() {
             <LandingPageCommonSection
                 title={"Team Velocity Suffers"}
                 textSections={[
-                    "Most of this work ends up falling on the project manager or the team lead. Instead of working with customers or on the product, they spend countless hours on administrative tasks to ensure other team members are productive.",
-                    "Even with all this effort, the complexity of taking in stakeholder input and applying it across hundreds of tasks leads to an imperfect board and team members unknowingly work on low priority tasks.",
+                    "Most of this work ends up falling on the project manager or the team lead. Instead of working on the product or spending time with customers, their time is consumed by administrative tasks.",
+                    "And in spite of their best efforts, prioritization and backlog grooming often take a back seat to other responsibilities. When this happens, team members unknowingly work on low priority tasks.",
                 ]}
                 placeContent="right"
                 wrappedButtonProps={wrappedButtonProps}
@@ -127,10 +127,10 @@ export function Home() {
             <LandingPageCommonSection
                 title={"Existing Solutions Are Limiting"}
                 textSections={[
-                    "The current solutions in the market can be put into two buckets: Brute Force Prioritization and Fixed Prioritization.",
-                    "In brute force solutions, each and every ticket needs to be dragged up or down in a column to be prioritized. Not only is this time consuming but it's nearly impossible to maintain a perfectly prioritized board across many columns, especially when priorties shift.",
-                    "In fixed solutions, the software provides a fixed list of tags like 'Low', 'Medium', and 'High'. When a project is small, this works great. But as the project grows, team members realize that 'Low' and 'Medium' tasks never get worked on so they add the 'High' priority to every task",
-                    "And when everything is everything is high priority, nothing is.",
+                    "Existing software solutions fall into one of two categories: Brute Force Prioritization and Fixed List Prioritization.",
+                    "In brute force solutions, every ticket is managed individually and needs to be dragged up or down in the column to communicate priority. Not only is this time consuming but it's nearly impossible to maintain a perfectly prioritized board.",
+                    "In fixed list solutions, the software provides a fixed list of tags like 'Low', 'Medium', and 'High'. When a project is small, this works great. But as the project grows, team members realize that 'Low' and 'Medium' tasks never get worked on so they begin adding the 'High' priority to every task",
+                    "Unfortunately, when everything is everything is a high priority nothing is.",
                 ]}
                 placeContent="left"
                 wrappedButtonProps={wrappedButtonProps}
@@ -139,8 +139,8 @@ export function Home() {
             <LandingPageCommonSection
                 title={"Introducing Relative Prioritization"}
                 textSections={[
-                    "Instead of having fixed lists or prioritizing each task individually, tasks can be prioritized in large groups with ticket tags.",
-                    "Custom tags are applied to each task and when the tags are prioritized, the backlog and all in progress tickets are adjusted to reflect the new priorities.",
+                    "Instead of having fixed lists or prioritizing each task individually, tasks can be prioritized in large chunks using dynamic tags.",
+                    "Custom tags are applied to each ticket to indicate the type of work that the ticket represents. After applying proper tags and prioritizing the tags against each other, the backlog and all in progress tickets automatically adjust to reflect the new priorities.",
                 ]}
                 placeContent="right"
                 wrappedButtonProps={wrappedButtonProps}
