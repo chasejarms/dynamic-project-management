250b8f5da682bf34539cfb9f0e291d4239235186
Chase <chasejarms@gmail.com>
2021-09-14 10:05:34 -0600

    Get rid of unused classes

--- src/oneTimeComponents/wrappedApp/app/authenticated/company/board/boardHome/index.tsx
+++ src/oneTimeComponents/wrappedApp/app/authenticated/company/board/boardHome/index.tsx
@@ -1,6 +1,5 @@
 /** @jsxImportSource @emotion/react */
 import { jsx, css } from "@emotion/react";
-import { Theme, useTheme } from "@material-ui/core";
 import { BoardColumnsContainer } from "../../../../../../../components/boardColumnsContainer";
 import { CenterLoadingSpinner } from "../../../../../../../components/centerLoadingSpinner";
 import { BoardContainer } from "../../../../../../../components/boardContainer";
@@ -17,8 +16,7 @@ import { TicketImages } from "../ticket/ticketImages";
 import { useBoardHomeLogic } from "../../../../../../../hooks/useBoardHomeLogic";
 
 export function BoardHome() {
-    const theme = useTheme();
-    const classes = createClasses(theme);
+    const classes = createClasses();
     const {
         isLoadingRequiredInformation,
         onUpdateTicket,
@@ -111,28 +109,7 @@ export function BoardHome() {
     );
 }
 
-const createClasses = (theme: Theme) => {
-    const centerContent = css`
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 16px;
-    `;
-
-    const toolbarContainer = css`
-        width: 100%;
-        display: flex;
-        justify-content: flex-start;
-        padding: 16px;
-        padding-left: 32px;
-        flex: 0 0 auto;
-    `;
-
-    const chipsContainer = css`
-        display: inline;
-    `;
-
+const createClasses = () => {
     const contentContainer = css`
         flex-grow: 1;
         display: flex;
@@ -140,62 +117,7 @@ const createClasses = (theme: Theme) => {
         position: relative;
     `;
 
-    const drawerContainer = css`
-        position: absolute;
-        right: 0;
-        z-index: 1;
-        display: grid;
-        grid-template-columns: auto 400px;
-        height: 100%;
-        width: 100%;
-    `;
-
-    const drawerDarkOpacityContainer = css`
-        background-color: rgba(0, 0, 0, 0.5);
-    `;
-
-    const drawerContentContainer = css`
-        background-color: white;
-        box-shadow: 0px 8px 10px -5px rgb(0 0 0 / 20%),
-            0px 16px 24px 2px rgb(0 0 0 / 14%),
-            0px 6px 30px 5px rgb(0 0 0 / 12%);
-        display: grid;
-        grid-template-rows: auto 1fr auto;
-    `;
-
-    const drawerHeaderContainer = css`
-        height: 60px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 0 8px 0 16px;
-        border-bottom: 1px solid rgba(0, 0, 0, 0.12);
-    `;
-
-    const drawerInnerContentContainer = css`
-        padding: 16px;
-    `;
-
-    const drawerActionButtonContainer = css`
-        border-top: 1px solid rgba(0, 0, 0, 0.12);
-        height: 60px;
-        overflow: auto;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 0 16px;
-    `;
-
     return {
-        centerContent,
-        toolbarContainer,
-        chipsContainer,
         contentContainer,
-        drawerContainer,
-        drawerDarkOpacityContainer,
-        drawerContentContainer,
-        drawerHeaderContainer,
-        drawerInnerContentContainer,
-        drawerActionButtonContainer,
     };
 };
