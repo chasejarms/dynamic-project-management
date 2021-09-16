917bbf9fb34e6f8d320ac27e13c861513b43b869
Chase <chasejarms@gmail.com>
2021-09-16 13:35:33 -0600

    Add the needed function to the tickets to augmented UI ticket util

--- src/utils/ticketsToAugmentedUITickets/index.ts
+++ src/utils/ticketsToAugmentedUITickets/index.ts
@@ -1,7 +1,7 @@
 import { ITicket } from "../../models/ticket";
 import { ITicketTemplate } from "../../models/ticketTemplate";
 import { IAugmentedUITicket } from "../../models/augmentedUITicket";
-import { createTicketPriorityScore } from "../createTicketPriorityScore";
+import mathEvaluator from "math-expression-evaluator";
 
 export function ticketsToAugmentedUITickets(
     tickets: ITicket[],
@@ -25,4 +25,30 @@ export function ticketsToAugmentedUITickets(
 
         return ticketForUI;
     });
+}
+
+function createTicketPriorityScore(
+    ticketTemplate: ITicketTemplate,
+    ticket: ITicket
+) {
+    if (!ticketTemplate.priorityWeightingCalculation.trim()) {
+        return -20000000;
+    }
+
+    let priorityWeightingCalculation =
+        ticketTemplate.priorityWeightingCalculation;
+    ticketTemplate.sections.forEach((section, index) => {
+        if (section.type === "number") {
+            if (!!section.alias) {
+                const sectionValue = ticket.sections[index];
+                priorityWeightingCalculation = priorityWeightingCalculation.replaceAll(
+                    section.alias,
+                    sectionValue.toString()
+                );
+            }
+        }
+    });
+
+    const priority = mathEvaluator.eval(priorityWeightingCalculation);
+    return Math.round(Number(priority));
 }
