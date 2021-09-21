bf8979b9825cadbc8ef481a3e2a76f335c346f7f
Chase <chasejarms@gmail.com>
2021-09-21 10:35:25 -0600

    Continue working on the useBoardColumnEditState hook

--- src/oneTimeComponents/wrappedApp/app/authenticated/company/board/boardEdit/hooks/useBoardColumnEditState/useBoardColumnEditState.test.tsx
+++ src/oneTimeComponents/wrappedApp/app/authenticated/company/board/boardEdit/hooks/useBoardColumnEditState/useBoardColumnEditState.test.tsx
@@ -3,10 +3,28 @@ import { useBoardColumnEditState } from ".";
 import { Provider as ReduxProvider } from "react-redux";
 import { createStore } from "../../../../../../../../../redux/store";
 import { MemoryRouter } from "react-router-dom";
+import { RouteCreator } from "../../../../../../utils/routeCreator";
+import { Api } from "../../../../../../../../../api";
+import { uncategorizedColumnReservedId } from "../../../../../../../../../constants/reservedColumnIds";
 
 describe("useBoardColumnEditState", () => {
     describe("the column data is not yet in redux", () => {
-        it("should return true for the isLoadingColumns key", () => {
+        it("should return true for the isLoadingColumns key", async () => {
+            jest.spyOn(Api.columns, "getColumns").mockImplementation(() => {
+                return Promise.resolve([
+                    {
+                        id: uncategorizedColumnReservedId,
+                        canBeModified: false,
+                        name: "Uncategorized",
+                    },
+                    {
+                        id: "1",
+                        canBeModified: true,
+                        name: "To Do",
+                    },
+                ]);
+            });
+
             const renderHookResult = renderHook(
                 () => {
                     return useBoardColumnEditState();
@@ -16,7 +34,9 @@ describe("useBoardColumnEditState", () => {
                         const store = createStore();
                         return (
                             <MemoryRouter
-                                initialEntries={["/my/initial/route"]}
+                                initialEntries={[
+                                    RouteCreator.editBoard("123", "456"),
+                                ]}
                             >
                                 <ReduxProvider store={store}>
                                     {children}
@@ -26,6 +46,62 @@ describe("useBoardColumnEditState", () => {
                     },
                 }
             );
+
+            expect(renderHookResult.result.current.isLoadingColumns).toBe(true);
+
+            await renderHookResult.waitForNextUpdate();
+
+            expect(renderHookResult.result.current.isLoadingColumns).toBe(
+                false
+            );
+        });
+    });
+
+    describe("the column data is already in redux", () => {
+        it("should not make the api request", () => {
+            const getColumnsSpy = jest
+                .spyOn(Api.columns, "getColumns")
+                .mockImplementation(() => {
+                    return Promise.resolve([
+                        {
+                            id: uncategorizedColumnReservedId,
+                            canBeModified: false,
+                            name: "Uncategorized",
+                        },
+                        {
+                            id: "1",
+                            canBeModified: true,
+                            name: "To Do",
+                        },
+                    ]);
+                });
+
+            const renderHookResult = renderHook(
+                () => {
+                    return useBoardColumnEditState();
+                },
+                {
+                    wrapper: ({ children }) => {
+                        const store = createStore();
+                        return (
+                            <MemoryRouter
+                                initialEntries={[
+                                    RouteCreator.editBoard("123", "456"),
+                                ]}
+                            >
+                                <ReduxProvider store={store}>
+                                    {children}
+                                </ReduxProvider>
+                            </MemoryRouter>
+                        );
+                    },
+                }
+            );
+
+            expect(renderHookResult.result.current.isLoadingColumns).toBe(
+                false
+            );
+            expect(getColumnsSpy.mock.calls.length).toBe(0);
         });
     });
 });
