ace208db690a88fb58794ac0eb5f09161b4b4351
Chase <chasejarms@gmail.com>
2021-09-23 13:23:25 -0600

    Move the drawer contents with action bar component to the new design system

--- src/oneTimeComponents/wrappedApp/app/authenticated/company/board/components/drawerContentsWithActionBar/index.tsx
+++ src/oneTimeComponents/wrappedApp/app/authenticated/company/board/components/drawerContentsWithActionBar/index.tsx
@@ -1,6 +1,4 @@
-/** @jsxImportSource @emotion/react */
-import { jsx, css } from "@emotion/react";
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 import {
     IWrappedButtonProps,
     WrappedButton,
@@ -16,20 +14,55 @@ export interface IDrawerContentsWithActionBarProps {
 export function DrawerContentsWithActionBar(
     props: IDrawerContentsWithActionBarProps
 ) {
-    const classes = createClasses();
     const wrappedButtonPropsExist =
         !!props.leftWrappedButtonProps || !!props.rightWrappedButtonProps;
 
     return (
-        <div css={classes.container}>
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                flexGrow: 1,
+            }}
+        >
             {!!props.title && (
-                <div css={classes.titleContainer}>
+                <Box
+                    sx={{
+                        borderBottom: "1px solid rgba(0, 0, 0, 0.12)",
+                        flex: "0 0 60px",
+                        display: "flex",
+                        py: 0,
+                        px: 2,
+                        justifyContent: "flex-start",
+                        alignItems: "center",
+                    }}
+                >
                     <Typography variant="h6">{props.title}</Typography>
-                </div>
+                </Box>
             )}
-            <div css={classes.contentContainer}>{props.children}</div>
+            <Box
+                sx={{
+                    padding: 2,
+                    flexGrow: 1,
+                    overflow: "auto",
+                    height: 0,
+                }}
+            >
+                {props.children}
+            </Box>
             {wrappedButtonPropsExist && (
-                <div css={classes.actionButtonContainer}>
+                <Box
+                    sx={{
+                        borderTop: "1px solid rgba(0, 0, 0, 0.12)",
+                        flex: "0 0 60px",
+                        overflow: "auto",
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        py: 0,
+                        px: 2,
+                    }}
+                >
                     <div>
                         {props.leftWrappedButtonProps?.map(
                             (wrappedButtonProps) => {
@@ -48,49 +81,8 @@ export function DrawerContentsWithActionBar(
                             }
                         )}
                     </div>
-                </div>
+                </Box>
             )}
-        </div>
+        </Box>
     );
 }
-
-const createClasses = () => {
-    const container = css`
-        display: flex;
-        flex-direction: column;
-        flex-grow: 1;
-    `;
-
-    const titleContainer = css`
-        border-bottom: 1px solid rgba(0, 0, 0, 0.12);
-        flex: 0 0 60px;
-        display: flex;
-        padding: 0 16px;
-        justify-content: flex-start;
-        align-items: center;
-    `;
-
-    const contentContainer = css`
-        padding: 16px;
-        flex-grow: 1;
-        overflow-y: auto;
-        height: 0px;
-    `;
-
-    const actionButtonContainer = css`
-        border-top: 1px solid rgba(0, 0, 0, 0.12);
-        flex: 0 0 60px;
-        overflow: auto;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 0 16px;
-    `;
-
-    return {
-        contentContainer,
-        actionButtonContainer,
-        container,
-        titleContainer,
-    };
-};
