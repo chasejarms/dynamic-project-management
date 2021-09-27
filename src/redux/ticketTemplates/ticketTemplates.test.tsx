deb35504c1a0c2f50e7302d57cc0c4b5cde5aa50
Chase <chasejarms@gmail.com>
2021-09-27 12:41:02 -0600

    Add tests for setting multiple ticket templates

--- src/redux/ticketTemplates/ticketTemplates.test.tsx
+++ src/redux/ticketTemplates/ticketTemplates.test.tsx
@@ -7,6 +7,7 @@ import weightedTicketTemplateCreationReducer, {
     resetWeightedTicketTemplateCreationState,
     setWeightedTicketTemplate,
     setWeightedTicketTemplateCreationFromExistingTicketTemplate,
+    setWeightedTicketTemplates,
 } from ".";
 import { ITicketTemplate } from "../../models/ticketTemplate";
 import { INumberSection } from "../../models/ticketTemplate/section/numberSection";
@@ -150,6 +151,153 @@ describe("Ticket Templates", () => {
 
             expect(priorityWeightingCalculation.value).toBe("");
             expect(priorityWeightingCalculation.error).toBe("");
+        });
+    });
+
+    describe("setWeightedTicketTemplates", () => {
+        it("should return the correct state", () => {
+            const clonedInitialState = cloneDeep(
+                initialTicketTemplateControlStateMapping
+            );
+
+            const textSection: ITextSection = {
+                type: "text",
+                label: "Text Label",
+                multiline: false,
+                required: false,
+            };
+
+            const numberSection: INumberSection = {
+                type: "number",
+                label: "Number Label",
+                required: false,
+                allowOnlyIntegers: false,
+                alias: "",
+            };
+
+            const ticketTemplateOne: ITicketTemplate = {
+                itemId: "",
+                belongsTo: "",
+                shortenedItemId: "123",
+                name: "Feature Template",
+                description: "Feature Description",
+                title: {
+                    label: "Title",
+                },
+                summary: {
+                    label: "Summary",
+                },
+                sections: [textSection, numberSection],
+                priorityWeightingCalculation: "",
+            };
+
+            const ticketTemplateTwo: ITicketTemplate = {
+                itemId: "",
+                belongsTo: "",
+                shortenedItemId: "234",
+                name: "Feature Template",
+                description: "Feature Description",
+                title: {
+                    label: "Title",
+                },
+                summary: {
+                    label: "Summary",
+                },
+                sections: [textSection, numberSection],
+                priorityWeightingCalculation: "",
+            };
+
+            const setWeightedTicketTemplatesAction = setWeightedTicketTemplates(
+                [ticketTemplateOne, ticketTemplateTwo]
+            );
+            const state = weightedTicketTemplateCreationReducer(
+                clonedInitialState,
+                setWeightedTicketTemplatesAction
+            );
+
+            const {
+                name,
+                description,
+                title,
+                summary,
+                sections: [
+                    ticketTemplateTextSectionControlState,
+                    ticketTemplateNumberSectionControlState,
+                ],
+                priorityWeightingCalculation,
+            } = state["123"];
+
+            expect(name.value).toBe("Feature Template");
+            expect(name.touched).toBe(false);
+            expect(name.error).toBe("");
+
+            expect(description.value).toBe("Feature Description");
+            expect(description.touched).toBe(false);
+            expect(description.error).toBe("");
+
+            expect(title.value).toBe("Title");
+            expect(title.touched).toBe(false);
+            expect(title.error).toBe("");
+
+            expect(summary.value).toBe("Summary");
+            expect(summary.touched).toBe(false);
+            expect(summary.error).toBe("");
+
+            const typedTextSection = ticketTemplateTextSectionControlState as ITicketTemplateTextSectionControlState;
+            expect(typedTextSection.error).toBe("");
+            expect(isEqual(typedTextSection.value, textSection)).toBe(true);
+
+            const typedNumberSection = ticketTemplateNumberSectionControlState as ITicketTemplateNumberSectionControlState;
+            expect(typedNumberSection.aliasError).toBe("");
+            expect(typedNumberSection.labelError).toBe("");
+            expect(typedNumberSection.maxError).toBe("");
+            expect(typedNumberSection.minError).toBe("");
+            expect(typedNumberSection.value).toBe(numberSection);
+
+            expect(priorityWeightingCalculation.value).toBe("");
+            expect(priorityWeightingCalculation.error).toBe("");
+
+            const {
+                name: nameTwo,
+                description: descriptionTwo,
+                title: titleTwo,
+                summary: summaryTwo,
+                sections: [
+                    ticketTemplateTwoTextSectionControlState,
+                    ticketTemplateTwoNumberSectionControlState,
+                ],
+                priorityWeightingCalculation: priorityWeightingCalculationTwo,
+            } = state["123"];
+
+            expect(nameTwo.value).toBe("Feature Template");
+            expect(nameTwo.touched).toBe(false);
+            expect(nameTwo.error).toBe("");
+
+            expect(descriptionTwo.value).toBe("Feature Description");
+            expect(descriptionTwo.touched).toBe(false);
+            expect(descriptionTwo.error).toBe("");
+
+            expect(titleTwo.value).toBe("Title");
+            expect(titleTwo.touched).toBe(false);
+            expect(titleTwo.error).toBe("");
+
+            expect(summaryTwo.value).toBe("Summary");
+            expect(summaryTwo.touched).toBe(false);
+            expect(summaryTwo.error).toBe("");
+
+            const typedTextSectionTwo = ticketTemplateTwoTextSectionControlState as ITicketTemplateTextSectionControlState;
+            expect(typedTextSectionTwo.error).toBe("");
+            expect(isEqual(typedTextSectionTwo.value, textSection)).toBe(true);
+
+            const typedNumberSectionTwo = ticketTemplateTwoNumberSectionControlState as ITicketTemplateNumberSectionControlState;
+            expect(typedNumberSectionTwo.aliasError).toBe("");
+            expect(typedNumberSectionTwo.labelError).toBe("");
+            expect(typedNumberSectionTwo.maxError).toBe("");
+            expect(typedNumberSectionTwo.minError).toBe("");
+            expect(typedNumberSectionTwo.value).toBe(numberSection);
+
+            expect(priorityWeightingCalculationTwo.value).toBe("");
+            expect(priorityWeightingCalculationTwo.error).toBe("");
         });
     });
 });
