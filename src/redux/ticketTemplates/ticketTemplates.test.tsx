58522c16fad563f13d4dfd043535c4e26e3f43c7
Chase <chasejarms@gmail.com>
2021-09-27 12:35:58 -0600

    Add in ticket templates tests

--- src/redux/ticketTemplates/ticketTemplates.test.tsx
+++ src/redux/ticketTemplates/ticketTemplates.test.tsx
@@ -2,9 +2,15 @@ import { cloneDeep, isEqual } from "lodash";
 import weightedTicketTemplateCreationReducer, {
     createTicketTemplateId,
     initialTicketTemplateControlStateMapping,
+    ITicketTemplateNumberSectionControlState,
+    ITicketTemplateTextSectionControlState,
     resetWeightedTicketTemplateCreationState,
+    setWeightedTicketTemplate,
     setWeightedTicketTemplateCreationFromExistingTicketTemplate,
 } from ".";
+import { ITicketTemplate } from "../../models/ticketTemplate";
+import { INumberSection } from "../../models/ticketTemplate/section/numberSection";
+import { ITextSection } from "../../models/ticketTemplate/section/textSection";
 
 describe("Ticket Templates", () => {
     describe("resetWeightedTicketTemplateCreationState", () => {
@@ -55,6 +61,95 @@ describe("Ticket Templates", () => {
                 "A New Description Label"
             );
             expect(createTicketTemplateState.name.value).toBe("Name (Copy)");
+        });
+    });
+
+    describe("setWeightedTicketTemplate", () => {
+        it("should return the correct state", () => {
+            const clonedInitialState = cloneDeep(
+                initialTicketTemplateControlStateMapping
+            );
+
+            const textSection: ITextSection = {
+                type: "text",
+                label: "Text Label",
+                multiline: false,
+                required: false,
+            };
+
+            const numberSection: INumberSection = {
+                type: "number",
+                label: "Number Label",
+                required: false,
+                allowOnlyIntegers: false,
+                alias: "",
+            };
+
+            const ticketTemplate: ITicketTemplate = {
+                itemId: "",
+                belongsTo: "",
+                shortenedItemId: "123",
+                name: "Feature Template",
+                description: "Feature Description",
+                title: {
+                    label: "Title",
+                },
+                summary: {
+                    label: "Summary",
+                },
+                sections: [textSection, numberSection],
+                priorityWeightingCalculation: "",
+            };
+            const setWeightedTicketTemplateAction = setWeightedTicketTemplate({
+                ticketTemplate,
+                ticketTemplateId: "123",
+            });
+            const state = weightedTicketTemplateCreationReducer(
+                clonedInitialState,
+                setWeightedTicketTemplateAction
+            );
+
+            const {
+                name,
+                description,
+                title,
+                summary,
+                sections: [
+                    ticketTemplateTextSectionControlState,
+                    ticketTemplateNumberSectionControlState,
+                ],
+                priorityWeightingCalculation,
+            } = state["123"];
+
+            expect(name.value).toBe("Feature Template");
+            expect(name.touched).toBe(false);
+            expect(name.error).toBe("");
+
+            expect(description.value).toBe("Feature Description");
+            expect(description.touched).toBe(false);
+            expect(description.error).toBe("");
+
+            expect(title.value).toBe("Title");
+            expect(title.touched).toBe(false);
+            expect(title.error).toBe("");
+
+            expect(summary.value).toBe("Summary");
+            expect(summary.touched).toBe(false);
+            expect(summary.error).toBe("");
+
+            const typedTextSection = ticketTemplateTextSectionControlState as ITicketTemplateTextSectionControlState;
+            expect(typedTextSection.error).toBe("");
+            expect(isEqual(typedTextSection.value, textSection)).toBe(true);
+
+            const typedNumberSection = ticketTemplateNumberSectionControlState as ITicketTemplateNumberSectionControlState;
+            expect(typedNumberSection.aliasError).toBe("");
+            expect(typedNumberSection.labelError).toBe("");
+            expect(typedNumberSection.maxError).toBe("");
+            expect(typedNumberSection.minError).toBe("");
+            expect(typedNumberSection.value).toBe(numberSection);
+
+            expect(priorityWeightingCalculation.value).toBe("");
+            expect(priorityWeightingCalculation.error).toBe("");
         });
     });
 });
